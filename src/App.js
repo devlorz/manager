134661fb68dc03083e33a3e465901bfc2800c249
Nutti Saelor <leelorz6@gmail.com>
2018-05-24 23:59:19 +0700

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,35 @@
+// @flow
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import config from '../node_modules/config';
+import firebase from 'firebase';
+
+type Props = {};
+export default class App extends Component<Props> {
+  componentDidMount() {
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Hello!</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  }
+});
